a63eef4e02f1213d9b2bd76782be0127266fd2d4
Pavel Dikunets <biodubstep@gmail.com>
2025-08-02 19:05:43 +0300

    Добавил регистрацию компонентов

---
+++ src/BirthdayManager/Infrastructure/BirthdayManager.Infrastructure.ComponentRegistrar/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using BirthdayManager.Application.AppData.Contexts.Contacts.Repositories;
+using BirthdayManager.Application.AppData.Contexts.Contacts.Services;
+using BirthdayManager.Infrastructure.Base;
+using BirthdayManager.Infrastructure.DataAccess;
+using BirthdayManager.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BirthdayManager.Infrastructure.ComponentRegistrar;
+
+/// <summary>
+/// Регистрация компонентов приложения в контейнере DI.
+/// </summary>
+public static class DependencyInjection
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IContactService, ContactService>();
+    }
+
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IBaseRepository<,>), typeof(Repository<,>));
+        services.AddScoped<IContactRepository, ContactRepository>();
+    }
+
+    public static void AddDbContext(this IServiceCollection services, string? connectionString)
+    {
+        services.AddDbContext<BirthdayManagerDbContext>(options => options.UseNpgsql(connectionString));
+    }
+}
