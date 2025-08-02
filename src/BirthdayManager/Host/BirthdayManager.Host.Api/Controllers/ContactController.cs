2a351dc69f74b86dd60e2b96dff6245939bdd4d9
Pavel Dikunets <biodubstep@gmail.com>
2025-08-02 19:06:06 +0300

    Добавил Api

---
+++ src/BirthdayManager/Host/BirthdayManager.Host.Api/Controllers/ContactController.cs
@@ -0,0 +1,34 @@
+﻿using BirthdayManager.Application.AppData.Contexts.Contacts.Services;
+using BirthdayManager.Contracts.Contexts.Contacts.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayManager.Host.Api.Controllers;
+
+/// <summary>
+/// Контроллер для работы с контактами.
+/// </summary>
+[ApiController]
+[Route("[controller]")]
+public class ContactController : ControllerBase
+{
+    private readonly IContactService _contactService;
+
+    /// <summary>
+    /// Инициализирует экземпляр <see cref="ContactController"/>.
+    /// </summary>
+    /// <param name="contactService">Сервис контактов.</param>
+    public ContactController(IContactService contactService)
+    {
+        _contactService = contactService;
+    }
+
+    [HttpGet("contacts/all")]
+    [ProducesResponseType(typeof(ContactResponseDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAllContactsAsync(CancellationToken cancellationToken)
+    {
+        var contacts = await _contactService.GetAllAsync(cancellationToken);
+
+        return Ok(contacts);
+    }
+}
---
+++ src/BirthdayManager/Host/BirthdayManager.Host.Api/Program.cs
@@ -0,0 +1,22 @@
+using BirthdayManager.Infrastructure.ComponentRegistrar;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplicationServices();
+builder.Services.AddRepositories();
+builder.Services.AddDbContext(builder.Configuration.GetConnectionString("Postgres"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
