9146fe061ddcb22d52bd7173e8ae25e00393cb6a
Pavel Dikunets <biodubstep@gmail.com>
2025-08-02 17:05:32 +0300

    Добавил базовый репозиторий

---
+++ src/BirthdayManager/Infrastructure/BirthdayManager.Infrastructure/Base/BaseRepository.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BirthdayManager.Infrastructure.Base;
+
+/// <inheritdoc />
+public class Repository<TEntity, TContext> : IBaseRepository<TEntity, TContext>
+    where TEntity : class where TContext : DbContext
+{
+    protected TContext DbContext;
+    protected DbSet<TEntity> DbSet;
+
+    /// <summary>
+    /// Инициализирует экземпляр <see cref="Repository{TEntity, TContext}"/>.
+    /// </summary>
+    /// <param name="dbContext">Контекст базы данных.</param>
+    public Repository(TContext dbContext)
+    {
+        DbContext = dbContext;
+        DbSet = DbContext.Set<TEntity>();
+    }
+
+    /// <inheritdoc />
+    public async Task AddAsync(TEntity model, CancellationToken cancellationToken)
+    {
+        await DbSet.AddAsync(model, cancellationToken);
+        await DbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public IQueryable<TEntity> GetAll()
+    {
+        return DbSet;
+    }
+
+    /// <inheritdoc />
+    public async Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var entity = await DbSet.FindAsync([id], cancellationToken);
+
+        if (entity == null)
+            throw new KeyNotFoundException($"Сущность с идентификатором '{id}' не найдена");
+
+        return entity;
+    }
+
+    /// <inheritdoc />
+    public IQueryable<TEntity> GetByPredicate(Expression<Func<TEntity, bool>> predicate)
+    {
+        return DbSet.Where(predicate);
+    }
+
+    /// <inheritdoc />
+    public async Task UpdateAsync(TEntity model, CancellationToken cancellationToken)
+    {
+        DbSet.Update(model);
+        await DbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    /// <inheritdoc />
+    public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var entity = await GetByIdAsync(id, cancellationToken);
+
+        DbSet.Remove(entity);
+        await DbContext.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/BirthdayManager/Infrastructure/BirthdayManager.Infrastructure/Base/IBaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BirthdayManager.Infrastructure.Base;
+
+/// <summary>
+/// Базовый репозиторий.
+/// </summary>
+/// <typeparam name="TEntity">Тип сущности.</typeparam>
+/// <typeparam name="TContext">Тип контекста.</typeparam>
+public interface IBaseRepository<TEntity, TContext> where TEntity : class where TContext : DbContext
+{
+    /// <summary>
+    /// Добавляет сущность.
+    /// </summary>
+    /// <param name="model">Модель сущности.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    Task AddAsync(TEntity model, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Получает все сущности.
+    /// </summary>
+    /// <returns>Все сущности.</returns>
+    IQueryable<TEntity> GetAll();
+
+    /// <summary>
+    /// Получает сущность по идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор сущности.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    /// <returns>Сущность.</returns>
+    Task<TEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Получает сущность по предикату.
+    /// </summary>
+    /// <param name="predicate">Предикат.</param>
+    /// <returns>Сущность.</returns>
+    IQueryable<TEntity> GetByPredicate(Expression<Func<TEntity, bool>> predicate);
+
+    /// <summary>
+    /// Обновляет сущность.
+    /// </summary>
+    /// <param name="model">Модель сущности.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    Task UpdateAsync(TEntity model, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Удаляет сущность по идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор сущности.</param>
+    /// <param name="cancellationToken">Токен отмены операции.</param>
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
