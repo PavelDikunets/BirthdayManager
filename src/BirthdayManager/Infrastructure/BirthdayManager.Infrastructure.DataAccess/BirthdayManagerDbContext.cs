7fbc9f4ca1526f7ec645d118bde46f803c2b04dc
Pavel Dikunets <biodubstep@gmail.com>
2025-08-02 17:45:58 +0300

    Добавил контекст базы данных

---
+++ src/BirthdayManager/Infrastructure/BirthdayManager.Infrastructure.DataAccess/BirthdayManagerDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BirthdayManager.Infrastructure.DataAccess.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace BirthdayManager.Infrastructure.DataAccess;
+
+/// <summary>
+/// Контекст базы данных.
+/// </summary>
+public class BirthdayManagerDbContext : DbContext
+{
+    /// <summary>
+    /// Инициализирует экземпляр <see cref="BirthdayManagerDbContext"/>.
+    /// </summary>
+    /// <param name="options">Опции.</param>
+    public BirthdayManagerDbContext(DbContextOptions<BirthdayManagerDbContext> options) : base(options)
+    {
+    }
+
+    /// <summary>
+    /// Настраивает модель базы данных и конфигурации сущностей.
+    /// </summary>
+    /// <param name="modelBuilder"></param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new ContactConfiguration());
+    }
+}
