4955866c24a29dc0192636a90132e05401a1bbe0
Pavel Dikunets <biodubstep@gmail.com>
2025-08-02 22:29:18 +0300

    Добавил модель ошибки

---
+++ src/BirthdayManager/Contracts/BirthdayManager.Contracts/Common/ErrorDto.cs
@@ -0,0 +1,17 @@
+﻿namespace BirthdayManager.Contracts.Common;
+
+/// <summary>
+/// Модель ошибки.
+/// </summary>
+public class ErrorDto
+{
+    /// <summary>
+    /// Сообщение об ошибке.
+    /// </summary>
+    public string Message { get; set; }
+
+    /// <summary>
+    /// Подробные детали ошибки.
+    /// </summary>
+    public string Details { get; set; }
+}
