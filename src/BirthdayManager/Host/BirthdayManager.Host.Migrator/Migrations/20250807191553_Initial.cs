ee1919e039af37b7346c3517a8a3d75fbaab425b
Pavel Dikuents <dikunets.pavel@gmail.com>
2025-08-07 23:51:51 +0300

    Добавил миграцию

--- src/BirthdayManager/Host/BirthdayManager.Host.Migrator/Migrations/20250807191553_Initial.cs
+++ src/BirthdayManager/Host/BirthdayManager.Host.Migrator/Migrations/20250807191553_Initial.cs
@@ -3,10 +3,10 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace BirthdayManager.Infrastructure.DataAccess.Migrations
+namespace BirthdayManager.Host.Migrator.Migrations
 {
     /// <inheritdoc />
-    public partial class Init : Migration
+    public partial class Initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
